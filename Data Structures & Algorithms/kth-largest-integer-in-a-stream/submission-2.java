601d8c13aef68888e5d854293a9f55d54810b8a5
Anish Agarwal <anishagarwal2502@gmail.com>
2026-04-21 22:58:00 +0530

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.java
@@ -0,0 +1,31 @@
+class KthLargest {
+
+    private PriorityQueue<Integer> heap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        heap = new PriorityQueue(k);
+        this.k = k;
+        for(int i = 0; i<Math.min(k,nums.length) ; i++){
+            heap.add(nums[i]);
+        }
+        for(int i = k; i< nums.length ; i++){
+           if(heap.peek()<nums[i]){
+                heap.poll();
+                heap.add(nums[i]);
+            }
+        }
+    }
+    
+    public int add(int val) {
+
+        if(heap.size()<this.k){
+            heap.add(val);
+        }else if(heap.peek()<val){
+            heap.poll();
+            heap.add(val);
+        }
+
+        return heap.peek();
+    }
+}
